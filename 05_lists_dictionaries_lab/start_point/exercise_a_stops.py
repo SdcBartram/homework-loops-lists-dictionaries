1d4d8ccb7b44c494eb6e485c7f9c7d1e108b075d
Sam <sdcbartram@icloud.com>
2023-04-04 16:42:46 +0100

    second commit exercise a completed

--- 05_lists_dictionaries_lab/start_point/exercise_a_stops.py
+++ 05_lists_dictionaries_lab/start_point/exercise_a_stops.py
@@ -1,19 +1,36 @@
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+stops = ["Croy", "Cumbernauld", "Falkirk High",
+         "Linlithgow", "Livingston", "Haymarket"]
 
-#1. Add "Edinburgh Waverley" to the end of the list
-stops.append("Edinburgh Waverley") 
-#to check
-# print(stops)
-#2. Add "Glasgow Queen St" to the start of the list
+# 1. Add "Edinburgh Waverley" to the end of the list
+stops.append("Edinburgh Waverley")
+
+# 2. Add "Glasgow Queen St" to the start of the list
 stops.insert(0, "Glasgow Queen St")
-#to check
-print(stops)
-#3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 
-#4. Print out the index position of "Linlithgow"
-#5. Remove "Livingston" from the list using its name
-#6. Delete "Cumbernauld" from the list by index
-#7. Print the number of stops there are in the list
-#8. Sort the list alphabetically
-#9. Reverse the positions of the stops in the list
-#10 Print out all the stops using a for loop
+# 3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
+# to check
+# print(stops)
+
+# 4. Print out the index position of "Linlithgow"
+index_position = stops.index("Linlithgow")
+print(index_position)
+
+# 5. Remove "Livingston" from the list using its name
+stops.remove("Livingston")
+
+# 6. Delete "Cumbernauld" from the list by index
+stops.pop(2)
+
+# 7. Print the number of stops there are in the list
+print(len(stops))
+
+# 8. Sort the list alphabetically
+stops.sort()
+
+# 9. Reverse the positions of the stops in the list
+stops.reverse()
+
+# 10 Print out all the stops using a for loop
+for stop in stops:
+    print(stop)
