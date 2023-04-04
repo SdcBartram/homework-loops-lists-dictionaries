d838dc3ac66c4b099a6d5a6cf80b023b27d66815
Sam <sdcbartram@icloud.com>
2023-04-04 20:56:31 +0100

    attempt at exercise d

--- 05_lists_dictionaries_lab/start_point/exercise_d_advanced_logic.py
+++ 05_lists_dictionaries_lab/start_point/exercise_d_advanced_logic.py
@@ -3,15 +3,26 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
+even_numbers = []
 
+for num in numbers:
+    if num % 2 == 0:
+        even_numbers.append(num)
+print(even_numbers)
 
 # 2. Print the difference between the largest and smallest value:
+ordered_numbers = sorted(numbers)
+# print(ordered_numbers)
+difference_of_max_and_min_number = ordered_numbers[-1] - ordered_numbers[0]
+print(difference_of_max_and_min_number)
 
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
 
 
 # 4. Print the sum of the numbers, 
+# # sum_numbers = sum(numbers)
+# # print(sum_numbers)
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
